bae667d6abdb11840f2a81e3eb61dc44a0a8348e
hiroki199990625 <hiroki19990625@gmail.com>
2022-12-20 22:32:25 +0900

    MasterBuilder > EditorOptionsを追加

---
+++ Packages/MasterBuilder/Editor/EditorOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterBuilder.Editor
+{
+    public static class EditorOptions
+    {
+        public static bool IsEnableAutoRegisterType { get; set; } = true;
+        public static bool IsEnableAutoGenerateSheet { get; set; } = true;
+    }
+}
