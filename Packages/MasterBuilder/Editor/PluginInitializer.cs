e76c73c08d9f9d094695df40b6a42eeb78211e11
hiroki199990625 <hiroki19990625@gmail.com>
2022-12-21 22:33:19 +0900

    MasterBuilder > アクセシビリティ変更

--- Packages/MasterBuilder/Editor/PluginInitializer.cs
+++ Packages/MasterBuilder/Editor/PluginInitializer.cs
@@ -5,19 +5,19 @@ namespace MasterBuilder.Editor
     public static class PluginInitializer
     {
         [InitializeOnLoadMethod]
-        public static void Initialize()
+        private static void Initialize()
         {
             AssemblyReloadEvents.afterAssemblyReload += AfterAssemblyReload;
         }
 
         [MenuItem("Master/Generate Xlsx Sheet")]
-        public static void GenerateSheet()
+        private static void GenerateSheet()
         {
             MasterSheetGenerator.Generate(MasterRegistry.MasterTypes);
         }
 
         [MenuItem("Master/Reimport Xlsx Sheet")]
-        public static void ReImportSheet()
+        private static void ReImportSheet()
         {
         }
 
