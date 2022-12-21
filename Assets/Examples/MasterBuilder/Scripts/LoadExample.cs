fd9ca9b7ea5ca8d734877acc382f42791f43f8a8
hiroki199990625 <hiroki19990625@gmail.com>
2022-12-21 22:49:48 +0900

    MasterBuilder > サンプル追加

---
+++ Assets/Examples/MasterBuilder/Scripts/LoadExample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MasterBuilder.Examples
+{
+    public static class LoadExample
+    {
+        [RuntimeInitializeOnLoadMethod]
+        public static void Initialize()
+        {
+            var stringCollections = Resources.LoadAll<ExampleLocalizeStringCollection>("Masters/Master");
+            var itemCollection = Resources.Load<ItemCollection>("Masters/Master2");
+        }
+    }
+}
