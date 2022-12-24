697d9872bfaa7f5ec3333f00cac1ddc3f6c582d7
hiroki199990625 <hiroki19990625@gmail.com>
2022-12-25 05:10:51 +0900

    MasterBuilder > 拡張メソッド追加

---
+++ Packages/MasterBuilder/Scripts/Extensions/CollectionContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MasterBuilder.Extensions
+{
+    public static class CollectionContextExtensions
+    {
+        public static IMasterCollection<TKey, TMasterDefine>
+            Context<T, TKey, TMasterDefine>(this IEnumerable<T> contexts, string contextName)
+            where T : ScriptableObject, IMasterCollection<TKey, TMasterDefine>
+            where TKey : IComparable<TKey>, IEquatable<TKey>
+            where TMasterDefine : MasterDefinition<TKey>
+        {
+            return contexts.FirstOrDefault(e =>
+            {
+                var splitName = e.name.Split("_");
+                if (splitName.Length != 2)
+                    return false;
+
+                return splitName[1] == contextName;
+            });
+        }
+    }
+}
