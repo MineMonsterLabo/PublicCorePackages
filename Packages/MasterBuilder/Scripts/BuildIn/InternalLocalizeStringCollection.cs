b9a5730a573cac2592b83b5811a716e577ba2074
hiroki199990625 <hiroki19990625@gmail.com>
2022-12-25 00:31:52 +0900

    MasterBuilder > ローカライズのマスタ追加

---
+++ Packages/MasterBuilder/Scripts/BuildIn/InternalLocalizeStringCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MasterBuilder.Attributes;
+
+namespace MasterBuilder.BuildIn
+{
+    public class InternalLocalizeStringCollection : LocalizeStringCollection<InternalLocalizeString>
+    {
+    }
+
+    [Serializable]
+    [MasterAsset(AssetPath = "Assets/Resources/MasterBuilder/Localize")]
+    [Master(Contexts = new[] { "en-US", "ja-JP" })]
+    public class InternalLocalizeString : LocalizeString
+    {
+    }
+}
