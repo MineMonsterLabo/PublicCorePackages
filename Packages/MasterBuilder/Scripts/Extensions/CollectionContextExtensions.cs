a8f3200dc2981fd958242a87b3fae362ec14dbae
hiroki199990625 <hiroki19990625@gmail.com>
2022-12-25 23:10:18 +0900

    MasterBuilder > CultureContext追加とパラメータのデフォルト値を設定

--- Packages/MasterBuilder/Scripts/Extensions/CollectionContextExtensions.cs
+++ Packages/MasterBuilder/Scripts/Extensions/CollectionContextExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using UnityEngine;
 
@@ -8,7 +9,7 @@ namespace MasterBuilder.Extensions
     public static class CollectionContextExtensions
     {
         public static IMasterCollection<TKey, TMasterDefine>
-            Context<T, TKey, TMasterDefine>(this IEnumerable<T> contexts, string contextName)
+            Context<T, TKey, TMasterDefine>(this IEnumerable<T> contexts, string contextName = "default")
             where T : ScriptableObject, IMasterCollection<TKey, TMasterDefine>
             where TKey : IComparable<TKey>, IEquatable<TKey>
             where TMasterDefine : MasterDefinition<TKey>
@@ -20,6 +21,24 @@ namespace MasterBuilder.Extensions
                     return false;
 
                 return splitName[1] == contextName;
+            });
+        }
+
+        public static IMasterCollection<TKey, TMasterDefine>
+            CultureContext<T, TKey, TMasterDefine>(this IEnumerable<T> contexts, CultureInfo cultureInfo = null)
+            where T : ScriptableObject, IMasterCollection<TKey, TMasterDefine>
+            where TKey : IComparable<TKey>, IEquatable<TKey>
+            where TMasterDefine : MasterDefinition<TKey>
+        {
+            cultureInfo ??= CultureInfo.CurrentCulture;
+
+            return contexts.FirstOrDefault(e =>
+            {
+                var splitName = e.name.Split("_");
+                if (splitName.Length != 2)
+                    return false;
+
+                return splitName[1] == cultureInfo.Name;
             });
         }
     }
